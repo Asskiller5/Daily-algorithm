982d8eca280e8d740830079c841db48cf216cb2f
Li Enqi <2768486573@qq.com>
2026-06-28 16:32:05 +0800

    Delete Leetcode1.py

--- aaa/Leetcode1.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.cn/problems/two-sum/
-class Solution:
-    def twoSum(self, nums, target):
-        seen = {}
-        for i, num in enumerate(nums):
-            if target - num in seen:
-                return [seen[target - num], i]
-            seen[num] = i
