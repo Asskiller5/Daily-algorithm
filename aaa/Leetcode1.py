63cc3b04c4bcbaa27c1fc0ce1b38de97fba04b98
Li Enqi <2768486573@qq.com>
2026-06-28 15:08:26 +0800

    第8个题解

---
+++ aaa/Leetcode1.py
@@ -0,0 +1,8 @@
+#https://leetcode.cn/problems/two-sum/
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            seen[num] = i
